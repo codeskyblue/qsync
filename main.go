2d53d017c0388c2127ca3530c3ed9fbb165f9a72
hzsunshx <hzsunshx@corp.netease.com>
2015-09-09 09:46:48 +0800

    change to multi thread upload

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/qiniu/api.v6/conf"
 	"github.com/qiniu/api.v6/io"
@@ -46,19 +47,25 @@ func uploadFile(bucket, key, filename string) error {
 func syncDir(bucket, keyPrefix, dir string) int {
 	keyPrefix = strings.TrimPrefix(keyPrefix, "/")
 	errCount := 0
+	wg := &sync.WaitGroup{}
 	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if info.IsDir() {
 			return nil
 		}
 		rel, _ := filepath.Rel(dir, path)
 		key := filepath.Join(keyPrefix, rel)
-		log.Printf("upload to %v ...", strconv.Quote(key))
-		if err := uploadFile(bucket, key, path); err != nil {
-			errCount += 1
-			log.Println(path, info)
-		}
+		wg.Add(1)
+		go func() {
+			log.Printf("Upload %v ...", strconv.Quote(key))
+			if err := uploadFile(bucket, key, path); err != nil {
+				errCount += 1
+				log.Printf("Failed %v, %v", strconv.Quote(path), err)
+			}
+			wg.Done()
+		}()
 		return nil
 	})
+	wg.Wait()
 	return errCount
 }
 
@@ -73,20 +80,7 @@ func main() {
 	conf.ACCESS_KEY = cfg.Qiniu.AccessKey
 	conf.SECRET_KEY = cfg.Qiniu.SecretKey
 	conf.UP_HOST = cfg.Qiniu.UpHost
-	log.Printf("Use up-host: %v", conf.UP_HOST)
+	log.Printf("Use upload host: %v", conf.UP_HOST)
 
 	syncDir(cfg.Qiniu.Bucket, cfg.Qiniu.KeyPrefix, cfg.Local.SyncDir)
-	/*
-		finfos, err := ioutil.ReadDir(cfg.Local.SyncDir)
-		if err != nil {
-			log.Fatal(err)
-		}
-		for _, finfo := range finfos {
-			log.Printf("Upload %v ...", finfo.Name())
-			log.Println(finfo)
-		}
-		if err := uploadFile(cfg.Qiniu.Bucket, "gorelease/", "main.go"); err != nil {
-			log.Println(err)
-		}
-	*/
 }
